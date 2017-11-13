7cc723f5eb079745023f7ab0fccd9b0019d5536a
Adam Poppenheimer <themakerofworlds@gmail.com>
2017-11-13 09:40:53 -0800

    Initializes the Unity project and sets up all the plugins/libraries I expect to use

---
+++ Civ Clone/Assets/Editor/TestUnitTest.cs
@@ -0,0 +1,12 @@
+using Zenject;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestUnitTest : ZenjectUnitTestFixture
+{
+    [Test]
+    public void RunTest1()
+    {
+        // TODO
+    }
+}
---
+++ Civ Clone/Assets/Plugins/Zenject/OptionalExtras/AutoMocking/AutoMocking/Editor/ReadMe.txt
@@ -0,0 +1,3 @@
+
+To fix compile errors, unzip the moq zip that applies to the Scripting Runtime Version you are using in your Player Settings.
+
