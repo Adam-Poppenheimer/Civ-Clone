14fd42c10bfd8cd1509bcc214525413213dd8492
Adam Poppenheimer <themakerofworlds@gmail.com>
2017-11-17 11:48:33 -0800

    Resolves the Building Possesson Canon issue

--- Assets/Cities/Buildings/BuildingPossessionCanon.cs
+++ Assets/Cities/Buildings/BuildingPossessionCanon.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+
+using UnityCustomUtilities.Extensions;
+
 using Assets.Cities;
 
 namespace Assets.Cities.Buildings {
@@ -11,7 +14,9 @@ namespace Assets.Cities.Buildings {
 
         #region instance fields and properties
 
+        private DictionaryOfLists<ICity, IBuilding> BuildingsInCity = new DictionaryOfLists<ICity, IBuilding>();
 
+        private Dictionary<IBuilding, ICity> CityOfBuilding = new Dictionary<IBuilding, ICity>();
 
         #endregion
 
@@ -20,27 +25,53 @@ namespace Assets.Cities.Buildings {
         #region from IBuildingPossessionCanon
 
         public bool CanPlaceBuildingInCity(IBuilding building, ICity city) {
-            throw new NotImplementedException();
-        }
+            if(building == null) {
+                throw new ArgumentNullException("building");
+            }else if(city == null) {
+                throw new ArgumentNullException("city");
+            }
 
-        public bool CanRemoveBuildingFromCity(IBuilding building, ICity city) {
-            throw new NotImplementedException();
+            return !CityOfBuilding.ContainsKey(building);
         }
 
         public ReadOnlyCollection<IBuilding> GetBuildingsInCity(ICity city) {
-            throw new NotImplementedException();
+            if(city == null) {
+                throw new ArgumentNullException("city");
+            }
+
+            return BuildingsInCity[city].AsReadOnly();
         }
 
         public ICity GetCityOfBuilding(IBuilding building) {
-            throw new NotImplementedException();
+            if(building == null) {
+                throw new ArgumentNullException("building");
+            }
+
+            ICity retval;
+            CityOfBuilding.TryGetValue(building, out retval);
+            return retval;
         }
 
         public void PlaceBuildingInCity(IBuilding building, ICity city) {
-            throw new NotImplementedException();
+            if(!CanPlaceBuildingInCity(building, city)) {
+                throw new InvalidOperationException("CanPlaceBuildingInCity must return true on the given arguments");
+            }
+
+            BuildingsInCity.AddElementToList(city, building);
+            CityOfBuilding[building] = city;
         }
 
         public void RemoveBuildingFromCurrentCity(IBuilding building) {
-            throw new NotImplementedException();
+            if(building == null) {
+                throw new ArgumentNullException("building");
+            }
+
+            var currentCity = GetCityOfBuilding(building);
+            if(currentCity != null) {
+                BuildingsInCity[currentCity].Remove(building);
+            }
+
+            CityOfBuilding[building] = null;
         }
 
         #endregion
