ab47391a578bac6f1151b82047c8afe8e4239a2f
Adam Poppenheimer <themakerofworlds@gmail.com>
2018-12-10 11:37:36 -0800

    Resolves issue #473

---
+++ Assets/UI/DropdownCloser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.UI {
+
+    public class DropdownCloser : MonoBehaviour {
+
+        #region instance methods
+
+        #region Unity messages
+
+        private void OnDisable() {
+            var dropdownList = transform.Find("Dropdown List");
+
+            if(dropdownList != null) {
+                Destroy(dropdownList.gameObject);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+
+}
