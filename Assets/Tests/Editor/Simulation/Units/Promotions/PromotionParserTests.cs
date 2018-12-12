deb47afb48a9598a1e7be87d10674d657204fa29
Adam Poppenheimer <themakerofworlds@gmail.com>
2018-12-12 11:57:54 -0800

    Resolves issue #479

--- Assets/Tests/Editor/Simulation/Units/Promotions/PromotionParserTests.cs
+++ Assets/Tests/Editor/Simulation/Units/Promotions/PromotionParserTests.cs
@@ -19,7 +19,10 @@ namespace Assets.Tests.Simulation.Units.Promotions {
 
         #region instance fields and properties
 
-        
+        private Mock<ICombatPromotionParser>   MockCombatParser;
+        private Mock<IMovementPromotionParser> MockMovementParser;
+        private Mock<IHealingPromotionParser>  MockHealingParser;
+        private Mock<IUnitPromotionLogic>      MockUnitPromotionLogic;
 
         #endregion
 
@@ -29,7 +32,17 @@ namespace Assets.Tests.Simulation.Units.Promotions {
 
         [SetUp]
         public void CommonInstall() {
+            MockCombatParser       = new Mock<ICombatPromotionParser>();
+            MockMovementParser     = new Mock<IMovementPromotionParser>();
+            MockHealingParser      = new Mock<IHealingPromotionParser>();
+            MockUnitPromotionLogic = new Mock<IUnitPromotionLogic>();
             
+            Container.Bind<ICombatPromotionParser>  ().FromInstance(MockCombatParser      .Object);
+            Container.Bind<IMovementPromotionParser>().FromInstance(MockMovementParser    .Object);
+            Container.Bind<IHealingPromotionParser> ().FromInstance(MockHealingParser     .Object);
+            Container.Bind<IUnitPromotionLogic>     ().FromInstance(MockUnitPromotionLogic.Object);
+
+            Container.Bind<PromotionParser>().AsSingle();
         }
 
         #endregion
@@ -37,15 +50,185 @@ namespace Assets.Tests.Simulation.Units.Promotions {
         #region tests
 
         [Test]
-        public void MissingTests() {
-            throw new NotImplementedException();
+        public void SetCombatSummary_ResetsSummary() {
+            var summary = new UnitCombatSummary();
+
+            summary.CanMoveAfterAttacking   = true;
+            summary.CanAttackAfterAttacking = true;
+            summary.BonusRange = 2;
+
+            var promotions = new List<IPromotion>();
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            promotionParser.SetCombatSummary(summary, promotions);
+
+            Assert.IsFalse(summary.CanMoveAfterAttacking);
+            Assert.IsFalse(summary.CanAttackAfterAttacking);
+            Assert.AreEqual(0, summary.BonusRange);
+        }
+
+        [Test]
+        public void SetCombatSummary_PassesSummaryAndEachPromotionIntoCombatParser() {
+            var summary = new UnitCombatSummary();
+
+            var promotions = new List<IPromotion>() {
+                BuildPromotion(), BuildPromotion(), BuildPromotion(),
+                BuildPromotion(), BuildPromotion(), BuildPromotion()
+            };
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            promotionParser.SetCombatSummary(summary, promotions);
+
+            foreach(var promotion in promotions) {
+                MockCombatParser.Verify(
+                    parser => parser.AddPromotionToCombatSummary(promotion, summary),
+                    Times.Once, "Failed to add a promotion to the combat summary"
+                );
+            }
+        }
+
+        [Test]
+        public void SetCombatSummary_SetsSummaryOnPromotionsOfUnit() {
+            var summary = new UnitCombatSummary();
+
+            var promotions = new List<IPromotion>() {
+                BuildPromotion(), BuildPromotion(), BuildPromotion(),
+                BuildPromotion(), BuildPromotion(), BuildPromotion()
+            };
+
+            var unit = BuildUnit(promotions);
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            promotionParser.SetCombatSummary(summary, unit);
+
+            foreach(var promotion in promotions) {
+                MockCombatParser.Verify(
+                    parser => parser.AddPromotionToCombatSummary(promotion, summary),
+                    Times.Once, "Failed to add a promotion to the combat summary"
+                );
+            }
+        }
+
+        [Test]
+        public void SetMovementSummary_ResetsSummary() {
+            var summary = new UnitMovementSummary();
+
+            summary.BonusMovement        = 2;
+            summary.CanTraverseDeepWater = true;
+            summary.CanTraverseLand      = true;
+
+            var promotions = new List<IPromotion>();
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            promotionParser.SetMovementSummary(summary, promotions);
+
+            Assert.AreEqual(0, summary.BonusMovement);
+            Assert.IsFalse(summary.CanTraverseDeepWater);
+            Assert.IsFalse(summary.CanTraverseLand);
+        }
+
+        [Test]
+        public void SetMovementSummary_PassesSummaryAndEachPromotionIntoMovementParser() {
+            var summary = new UnitMovementSummary();
+
+            var promotions = new List<IPromotion>() {
+                BuildPromotion(), BuildPromotion(), BuildPromotion(),
+                BuildPromotion(), BuildPromotion(), BuildPromotion()
+            };
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            promotionParser.SetMovementSummary(summary, promotions);
+
+            foreach(var promotion in promotions) {
+                MockMovementParser.Verify(
+                    parser => parser.AddPromotionToMovementSummary(promotion, summary),
+                    Times.Once, "Failed to add a promotion to the movement summary"
+                );
+            }
+        }
+
+        [Test]
+        public void SetMovementSummary_SetsSummaryOnPromotionsOfUnit() {
+            var summary = new UnitMovementSummary();
+
+            var promotions = new List<IPromotion>() {
+                BuildPromotion(), BuildPromotion(), BuildPromotion(),
+                BuildPromotion(), BuildPromotion(), BuildPromotion()
+            };
+
+            var unit = BuildUnit(promotions);
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            promotionParser.SetMovementSummary(summary, unit);
+
+            foreach(var promotion in promotions) {
+                MockMovementParser.Verify(
+                    parser => parser.AddPromotionToMovementSummary(promotion, summary),
+                    Times.Once, "Failed to add a promotion to the movement summary"
+                );
+            }
+        }
+
+        [Test]
+        public void GetVisionInfo_ThrowsNotImplementedException() {
+            var unit = BuildUnit(new List<IPromotion>());
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            Assert.Throws<NotImplementedException>(() => promotionParser.GetVisionInfo(unit));
+        }
+
+        [Test]
+        public void GetHealingInfo_ReturnsNonNullInfo() {
+            var unit = BuildUnit(new List<IPromotion>());
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            Assert.IsNotNull(promotionParser.GetHealingInfo(unit));
+        }
+
+        [Test]
+        public void GetHealingInfo_PassesReturnedInfoAndAllUnitPromotionsIntoHealingParser() {
+            var promotions = new List<IPromotion>() {
+                BuildPromotion(), BuildPromotion(), BuildPromotion(),
+                BuildPromotion(), BuildPromotion(), BuildPromotion()
+            };
+
+            var unit = BuildUnit(promotions);
+
+            var promotionParser = Container.Resolve<PromotionParser>();
+
+            var healingInfo = promotionParser.GetHealingInfo(unit);
+
+            foreach(var promotion in promotions) {
+                MockHealingParser.Verify(
+                    parser => parser.ParsePromotionForHealingInfo(promotion, unit, healingInfo),
+                    Times.Once, "Failed to parse a promotion for the HealingInfo"
+                );
+            }
         }
 
         #endregion
 
         #region utilities
         
+        private IPromotion BuildPromotion() {
+            return new Mock<IPromotion>().Object;
+        }
 
+        private IUnit BuildUnit(IEnumerable<IPromotion> promotions) {
+            var newUnit = new Mock<IUnit>().Object;
+
+            MockUnitPromotionLogic.Setup(logic => logic.GetPromotionsForUnit(newUnit)).Returns(promotions);
+
+            return newUnit;
+        }
 
         #endregion
 
